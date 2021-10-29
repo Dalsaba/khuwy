c167eb09422675fa6db9451fd6a2ce65a58e665d
Dalsaba <correialopesl@gmail.com>
2021-10-29 12:00:44 +0200

    lucytest3

---
+++ khuwy/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Your ';
+  isExpanded = false;
+  isShowing = false;
+  showSubmenu: boolean = false;
+
+  constructor() {
+  }
+
+  mouseenter() {
+    if (!this.isExpanded) {
+      this.isShowing = true;
+    }
+  }
+
+  mouseleave() {
+    if (!this.isExpanded) {
+      this.isShowing = false;
+    }
+}
+
+
+  }
+
---
+++ khuwy/src/app/component/information/information.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { MatCarousel, MatCarouselComponent } from '@ngmodule/material-carousel';
+
+
+
+@Component({
+  selector: 'app-information',
+  templateUrl: './information.component.html',
+  styleUrls: ['./information.component.css']
+})
+export class InformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  
+  slides = [
+    {'image': 'https://gsr.dev/material2-carousel/assets/demo.png'}, 
+    {'image': 'https://gsr.dev/material2-carousel/assets/demo.png'},
+    {'image': 'https://gsr.dev/material2-carousel/assets/demo.png'}, 
+    {'image': 'https://gsr.dev/material2-carousel/assets/demo.png'}, 
+    {'image': 'https://gsr.dev/material2-carousel/assets/demo.png'}
+  ];
+
+  
+  
+
+}
